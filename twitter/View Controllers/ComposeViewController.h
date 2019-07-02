2561b9373d20af0321794a8421ce8f73203f15e9
Emily Hsiao <emily13hsiao@fb.com>
2019-07-02 15:47:30 -0700

    Tweet page basic functionality.

---
+++ twitter/View Controllers/ComposeViewController.h
@@ -0,0 +1,18 @@
+//
+//  ComposeViewController.h
+//  twitter
+//
+//  Created by emily13hsiao on 7/2/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *composeTweetLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
